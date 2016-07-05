0d27c895582351b41b985a0f2ad93b21e367af40
zhanhun <zhanhun@mogujie.com>
2016-07-05 16:25:01 +0800

    链表逆序

--- sourceManager/sourceManager/main.m
+++ sourceManager/sourceManager/main.m
@@ -40,22 +40,92 @@ stu_t * create_node(void);
  */
 void add_node(stu_t *head,stu_t *node);
 
+
+/*!
+ *  @author Ryu, 16-07-05 12:07:59
+ *
+ *  @brief 打印用户信息
+ *
+ *  @param node <#node description#>
+ *
+ *  @since 1.0
+ */
+void printf_stu(stu_t *node);
+
+/*!
+ *  @author Ryu, 16-07-05 12:07:19
+ *
+ *  @brief 遍历
+ *
+ *  @param head <#head description#>
+ *  @param func <#func description#>
+ *
+ *  @since 1.0
+ */
+void travelAllNode(stu_t *head,void(*func)(stu_t *));
+
+/*!
+ *  @author Ryu, 16-07-05 12:07:33
+ *
+ *  @brief 展示所有用户信息
+ *
+ *  @param node <#node description#>
+ *
+ *  @since 1.0
+ */
+void show_stu_info(stu_t *node);
+
+/*!
+ *  @author Ryu, 16-07-05 15:07:35
+ *
+ *  @brief 链表逆序
+ *
+ *  @param head <#head description#>
+ *
+ *  @since 1.0
+ */
+stu_t * revertAllNode(stu_t *head);
+
 int main(void) {
 
     stu_t *head;
     head = create_node();
     
     while (1) {
-        printf("   **********************************\n   ***             menu           ***\n   ***       1.insert student     ***\n   ***       2.printf student     ***\n   **********************************\n");
+        printf("   **********************************\n   ***             menu           ***\n   ***       1.insert student     ***\n   ***       2.printf student     ***\n   ***       3.revert student     ***\n   ***       4.clear  student     ***\n   **********************************\n");
         int ctl;
         scanf("%d",&ctl);
-        if (1 == ctl) {
-            printf("点击的是1\n");
-            scanf("");
-        } else if (2 == ctl) {
-            printf("点击的是2\n");
-        } else {
-            printf("点击的是其他\n");
+        switch (ctl) {
+            case 1:
+            {
+                stu_t *node = create_node();
+                scanf("%s %d %d",node->name,&node->age,&node->memberNum);
+                add_node(head, node);
+                printf("录入成功\n");
+            }
+                break;
+            case 2:
+            {
+                show_stu_info(head);
+            }
+                break;
+            case 3:
+            {
+                stu_t *node = create_node();
+                add_node(head, node);
+                show_stu_info(revertAllNode(head)) ;
+                
+            }
+                break;
+            case 4:
+            {
+            }
+                break;
+            default:
+            {
+                printf("点击的是其他\n");
+            }
+                break;
         }
     }
     
@@ -66,13 +136,13 @@ int main(void) {
 
 stu_t * create_node(void) {
 
-    stu_t *head = (stu_t *)malloc(sizeof(stu_t));
-    if (!head) {
+    stu_t *node = (stu_t *)malloc(sizeof(stu_t));
+    if (!node) {
         perror("malloc");
         exit(-1);
     }
-    head->next = NULL;
-    return head;
+    node->next = NULL;
+    return node;
 
 }
 
@@ -81,4 +151,39 @@ void add_node(stu_t *head,stu_t *node) {
         head = head->next;
     }
     head->next = node;
+}
+
+stu_t * revertAllNode(stu_t *head) {
+    stu_t *p;
+    stu_t *r;
+    if (head->next && head->next->next) {
+        p = head->next;
+        r = p->next;
+        p->next = NULL;
+        head = p;
+        while (r) {
+            p = r;
+            r = r->next;
+            p->next = head;
+            head = p;
+        }
+        return head;
+    }
+    return head;
+}
+
+void travelAllNode(stu_t *head,void(*func)(stu_t *)) {
+    
+    while ( (head = head->next)) {
+        func(head);
+    }
+
+}
+
+void show_stu_info(stu_t *node) {
+    travelAllNode(node,printf_stu);
+}
+
+void printf_stu(stu_t *node) {
+    printf("name:%s age:%d memberNum:%d\n",node->name,node->age,node->memberNum);
 }
